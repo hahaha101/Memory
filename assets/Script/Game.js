1469b2e10af4b7a9a25b1f3893c83fc1eed0f993
liuleidong <liuleidong@junetech.com.cn>
2018-09-19 17:56:46 +0800

    增加分享按钮

--- assets/Script/Game.js
+++ assets/Script/Game.js
@@ -314,6 +314,56 @@ var Game = cc.Class({
     quitToMenu: function () {
         this.audioMng.playButton();
         cc.director.loadScene('menu');
+    },
+    onShareGame: function(){
+        if (typeof FBInstant === 'undefined') return;
+        let highScore = Number(cc.sys.localStorage.getItem('highScore0'));
+        var txt = "my best score is " + highScore + ",come and challenge me!"
+        FBInstant.shareAsync({
+            intent: 'SHARE',
+            image: this.getImgBase64(),
+            text: txt,
+            data: {myReplayData: '...'},
+        }).then(() => {
+            // continue with the game.
+        });
+    },
+    // 截屏返回 iamge base6，用于 Share
+    getImgBase64 () {
+
+        let target = cc.find('Canvas');
+        let width = 1334, height = 750;
+        let renderTexture = new cc.RenderTexture(width, height);
+        renderTexture.begin();
+        target._sgNode.visit();
+        renderTexture.end();
+        //
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+        canvas.width = width;
+        canvas.height = height;
+        if (cc._renderType === cc.game.RENDER_TYPE_CANVAS) {
+            let texture = renderTexture.getSprite().getTexture();
+            let image = texture.getHtmlElementObj();
+            ctx.drawImage(image, 0, 0);
+        }
+        else if (cc._renderType === cc.game.RENDER_TYPE_WEBGL) {
+            let buffer = gl.createFramebuffer();
+            gl.bindFramebuffer(gl.FRAMEBUFFER, buffer);
+            let texture = renderTexture.getSprite().getTexture()._glID;
+            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+            let data = new Uint8Array(width * height * 4);
+            gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, data);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+            let rowBytes = width * 4;
+            for (let row = 0; row < height; row++) {
+                let srow = height - 1 - row;
+                let data2 = new Uint8ClampedArray(data.buffer, srow * width * 4, rowBytes);
+                let imageData = new ImageData(data2, width, 1);
+                ctx.putImageData(imageData, 0, row);
+            }
+        }
+        return canvas.toDataURL('image/png');
     }
 
 });
--- assets/Script/Menu.js
+++ assets/Script/Menu.js
@@ -151,7 +151,11 @@ cc.Class({
         }
     },
     onQuit: function(){
-        cc.director.end();
+        if (typeof FBInstant === 'undefined'){
+            cc.director.end();
+        }else{
+            FBInstant.quit();
+        }
     },
 
     playGame: function (mode,customEventData) {
@@ -271,4 +275,52 @@ cc.Class({
     update: function (dt) {
 
     },
+    onShareGame: function(){
+        if (typeof FBInstant === 'undefined') return;
+        FBInstant.shareAsync({
+            intent: 'SHARE',
+            image: this.getImgBase64(),
+            text: 'Come and challenge my brain with me.',
+            data: {myReplayData: '...'},
+        }).then(() => {
+            // continue with the game.
+        });
+    },
+    // 截屏返回 iamge base6，用于 Share
+    getImgBase64 () {
+
+        let target = cc.find('Canvas');
+        let width = 1334, height = 750;
+        let renderTexture = new cc.RenderTexture(width, height);
+        renderTexture.begin();
+        target._sgNode.visit();
+        renderTexture.end();
+        //
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+        canvas.width = width;
+        canvas.height = height;
+        if (cc._renderType === cc.game.RENDER_TYPE_CANVAS) {
+            let texture = renderTexture.getSprite().getTexture();
+            let image = texture.getHtmlElementObj();
+            ctx.drawImage(image, 0, 0);
+        }
+        else if (cc._renderType === cc.game.RENDER_TYPE_WEBGL) {
+            let buffer = gl.createFramebuffer();
+            gl.bindFramebuffer(gl.FRAMEBUFFER, buffer);
+            let texture = renderTexture.getSprite().getTexture()._glID;
+            gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+            let data = new Uint8Array(width * height * 4);
+            gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, data);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+            let rowBytes = width * 4;
+            for (let row = 0; row < height; row++) {
+                let srow = height - 1 - row;
+                let data2 = new Uint8ClampedArray(data.buffer, srow * width * 4, rowBytes);
+                let imageData = new ImageData(data2, width, 1);
+                ctx.putImageData(imageData, 0, row);
+            }
+        }
+        return canvas.toDataURL('image/png');
+    }
 });
